2b5cec22f403f976ecb64fcc55b92d53e491d9d2
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-07-25 15:01:35 -0300

    Fix: ejercicio 6

--- ejercicio_6.cpp
+++ ejercicio_6.cpp
@@ -2,37 +2,44 @@
 /*
 Calificaciones segun nota
 */
+#include <cmath>
 #include <iostream>
 #include <string>
 
-int main (int argc, char *argv[]) {
-	int nota;
-	std::string msj;
+int main(int argc, char *argv[]) {
+  int nota;
+  std::string msj;
+  std::cout << "Ingresa una nota: ";
+  std::cin >> nota;
+  std::cout << std::endl;
 
-	std::cout << "Ingresa una nota: ";
-	std::cin >> nota;
-	std::cout << std::endl;
-	switch (nota) {
-		case (0, 1, 2, 3, 4):
-			msj = "Suspenso";
-		break;
-		case (5, 6):
-			msj = "Aprobado";
-		break;
-		case (7, 8):
-			msj = "Notable";
-		break;
-		case 9:
-			msj = "Sobresaliente";
-		break;
-		case 10:
-			msj = "Matricula de honor";
-		break;
-		default:
-			msj = "Error";
-		break;
-	}
+  switch (nota) {
+  case 0:
+  case 1:
+  case 2:
+  case 3:
+  case 4:
+    msj = "Suspenso";
+    break;
+  case 5:
+  case 6:
+    msj = "Aprobado";
+    break;
+  case 7:
+  case 8:
+    msj = "Notable";
+    break;
+  case 9:
+    msj = "Sobresaliente";
+    break;
+  case 10:
+    msj = "Matricula de honor";
+    break;
+  default:
+    msj = "Error";
+    break;
+  }
 
-	std::cout << "Calificaciones para ('" << nota << "'): " << msj << std::endl;
-	return 0;
+  std::cout << "Calificaciones para ('" << nota << "'): " << msj << std::endl;
+  return 0;
 }
