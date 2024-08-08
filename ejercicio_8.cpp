d1cf85695b32b11afc912dcf367cf14f00dc82bd
Fernando Pessoa <fernandomatiaspessoa471@gmail.com>
2024-08-08 05:27:15 -0300

    fix: ejercicio_8

--- ejercicio_8.cpp
+++ ejercicio_8.cpp
@@ -1,37 +1,43 @@
 /*
-Realizar un programa que, tras leer un número natural, escriba por pantalla los cinco
-primeros números impares que sean mayores que dicho número y múltiplo de tres.
-Por Ej. Si el usuario ingresa un 7 (siete), el programa deberá emitir el resultado:
-Los 5 Impares y Múltiplos de 3 a partir de 7 son: 9 - 15 - 21 - 27 - 33 
+Realizar un programa que, tras leer un número natural, escriba por pantalla los
+cinco primeros números impares que sean mayores que dicho número y múltiplo de
+tres. Por Ej. Si el usuario ingresa un 7 (siete), el programa deberá emitir el
+resultado: Los 5 Impares y Múltiplos de 3 a partir de 7 son: 9 - 15 - 21 - 27 -
+33
 */
 
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-    int numero, contador = 0, candidato;
+int main() {
+    int numero, contador = 0;
 
     std::cout << "Ingrese un número natural: ";
     std::cin >> numero;
 
     while (numero <= 0) {
-        std::cout << "El número ingresado no es válido. Debe ser un número natural." << std::endl;
-		std::cin >> numero;
+        std::cout
+            << "El número ingresado no es válido. Debe ser un número natural."
+            << std::endl;
+        std::cout << "Ingrese un número natural: ";
+        std::cin >> numero;
     }
 
-    std::cout << "Los 5 Impares y Múltiplos de 3 a partir de " << numero << " son: ";
+    int candidato = numero + (numero % 2 == 0 ? 1 : 2);
 
-	candidato = numero;
+    std::cout << "Los 5 Impares y Múltiplos de 3 a partir de " << numero
+              << " son: ";
 
     while (contador < 5) {
-		if (candidato % 2 != 0 && candidato % 3 == 0) {
-			std::cout << candidato;
-			if (contador < 4) {
-				std::cout << " - ";
-			}
-			contador++;
-		};
-		candidato += 1;
+        if (candidato % 3 == 0) {
+            std::cout << candidato;
+            if (contador < 4) {
+                std::cout << " - ";
+            }
+            contador++;
+        }
+        candidato += 2;
     }
+
     std::cout << std::endl;
     return 0;
 }
